21545609a5122691510df785646657b3c965ffcc
Baraa <huss.je@gmail.com>
2026-06-12 04:00:48 +0400

    Refine internal page back navigation

--- app/components/PageBackActions.tsx
+++ app/components/PageBackActions.tsx
@@ -1,20 +1,13 @@
 "use client";
 
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function PageBackActions() {
   const router = useRouter();
 
   return (
-    <div className="pageBackActions">
-      <button className="secondaryBtn" onClick={() => router.back()}>
-        ← Back
-      </button>
-
-      <Link href="/" className="secondaryBtn">
-        Home
-      </Link>
-    </div>
+    <button className="pageBackInlineBtn" onClick={() => router.back()}>
+      ← Back
+    </button>
   );
 }
