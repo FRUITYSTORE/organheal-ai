a905b472fb8171ecfa8fa71942942877b3ef4287
Hussam <huss.je@gmail.com>
2026-07-05 15:21:55 +0300

    Prepare blog page for shared layout

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import Link from "next/link";
+import PageLayout from "@/app/components/navigation/PageLayout";
 import { blogPosts } from "@/lib/blogData";
 
 type Language = "en" | "ar";
